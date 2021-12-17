572a64f41eaa943ef697fc77e34c76ec902bd225
dhir0hit <rohit786dhir@gmail.com>
2021-12-16 20:13:18 -0500

    new files

---
+++ Script/script.js
@@ -0,0 +1,9 @@
+var sidebar = document.getElementById("SideBar");
+
+function openSideBar() {
+    sidebar("SideBar").style.display = "block";
+}
+
+function closeSideBar() {
+    sidebar("SideBar").style.display = "none";
+}
