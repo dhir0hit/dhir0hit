c15d9464f9ea3a250c3945e57e0b63b89350ff54
dhir0hit <91639870+dhir0hit@users.noreply.github.com>
2021-12-16 20:25:53 -0500

    Delete script.js

--- Script/script.js
+++
@@ -1,9 +0,0 @@
-var sidebar = document.getElementById("SideBar");
-
-function openSideBar() {
-    sidebar("SideBar").style.display = "block";
-}
-
-function closeSideBar() {
-    sidebar("SideBar").style.display = "none";
-}
